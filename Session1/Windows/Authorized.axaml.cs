7fd2d477613c73df06f23d3bab4a9f2bcdbad3c2
ghfg <sdfsdf@.com>
2024-06-05 17:34:54 +0300

    05.06.2024/17:34

---
+++ Session1/Windows/Authorized.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Session1.Windows;
+
+public partial class Authorized : Window
+{
+    public Authorized()
+    {
+        InitializeComponent();
+    }
+}
--- Session1/Windows/MainWindow.axaml.cs
+++ Session1/Windows/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Session1.Windows;
 
 namespace Session1;
 
@@ -7,6 +9,12 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        
+        Enter.Click += EnterOnClick;
+    }
+
+    private void EnterOnClick(object? sender, RoutedEventArgs e)
+    {
+        Authorized atrd = new Authorized();
+        atrd.Show();
     }
 }
